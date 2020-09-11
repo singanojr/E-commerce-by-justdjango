efbf77ac88893741d6010a3257ed738764085323
osori <hyeseong43@gmail.com>
2020-09-11 16:58:30 +0900

    2nd commit

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, checkout, products
+from .views import HomeView, checkout, ItemDetailView, add_to_cart 
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', home, name='item-list'),
+    path('', HomeView.as_view(), name='item-list'),
     path('checkout/', checkout, name='checkout'),
-    path('products/', products, name='products'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
 ]
